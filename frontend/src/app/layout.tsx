a6b5908e0009666206123f97c9a327ce09055241
Alken Rrokaj <rrokajalken@gmail.com>
2025-06-01 20:04:08 +0200

    add vercel speed insights

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -17,6 +18,7 @@ export default function RootLayout({
       <body className={`antialiased min-h-screen bg-[#0a0303]`}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
